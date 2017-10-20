0a72977e4dd25304c1c7e694445f310261b9fc46
Vardan <31370786+VardanyanV@users.noreply.github.com>
2017-10-20 18:00:23 +0400

    Update UserNameValidator.js

--- UserNameValidator.js
+++ UserNameValidator.js
@@ -1,9 +1,10 @@
-const Validator =  require('./Validator.js');
+const Validator =  require("./Validator");
 class UsernameValidator extends Validator {
-    isUserName(user) {
+    static isUserName(user) {
         let userRegExp = /^[\w+_-]{4,24}$/;
         return !user || !super.isString(user) || userRegExp.test(user) ? false : true;
     }
 }
 
+console.log("User name is " + UsernameValidator.isString("VardanyanV"));
 module.exports = Validator;
