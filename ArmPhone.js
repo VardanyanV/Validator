090b4eaa036f2c8daac0938500f9c04feeb599cc
Vardan <31370786+VardanyanV@users.noreply.github.com>
2017-10-20 17:58:14 +0400

    Update ArmPhone.js

--- ArmPhone.js
+++ ArmPhone.js
@@ -1,12 +1,10 @@
-const Validator =  require('./Validator.js');
-class ArmPhoneValidator extends Validator{
-  isArmPhoneNumber(phone){
-    if(!phone || !super.isNumber(phone))
-      return false;
-    let phoneReg = /^(055\d{6}|095\d{6}|077\d{6}|093\d{6}|094\d{6}|098\d{6}|091\d{6}|099\d{6}|041\d{6}|043\d{6}|010\d{6}|[+]374\d{8}|[+]37410\d{6})$/;
-    if(phoneReg.test(phonenum))
-      return true;
-      return false;
+const Validator =  require("./Validator");
+class ArmPhoneValidator extends Validator {
+    static isArmPhoneNumber(phone) {
+        let phoneRegExp = /^(055\d{6}|095\d{6}|077\d{6}|093\d{6}|094\d{6}|098\d{6}|091\d{6}|099\d{6}|041\d{6}|043\d{6}|010\d{6}|[+]374\d{8}|[+]37410\d{6})$/;
+        (!phone || !super.isNumber(phone) || !phoneRegExp.test(phone)) ? false : true;
+    }
 }
-}
+
+console.log("Phone is " + ArmPhoneValidator.isNumber(+37499999192));
 module.exports = Validator;
